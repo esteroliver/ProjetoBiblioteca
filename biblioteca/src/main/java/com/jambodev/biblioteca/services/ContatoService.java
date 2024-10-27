7eac951dc499cf8d4a27eb9906ddbaa086c42634
esteroliver <oliver.ester.2004@gmail.com>
2024-10-27 10:41:39 -0300

    feat: services de contato e livro

--- biblioteca/src/main/java/com/jambodev/biblioteca/services/ContatoService.java
+++ biblioteca/src/main/java/com/jambodev/biblioteca/services/ContatoService.java
@@ -2,11 +2,48 @@ package com.jambodev.biblioteca.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import com.jambodev.biblioteca.repositories.ContatoRepository;
 import com.jambodev.biblioteca.entities.Contato;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContatoService{
     @Autowired
-    ContatoRepository repository;
+    ContatoRepository cr;
+
+    public List<Contato> listarContatos(){
+        return cr.findAll();
+    }
+
+    public Contato buscarContato(Long id) throws Exception {
+        Optional<Contato> contato_optional = cr.findById(id);
+        if(contato_optional.isEmpty()){
+            throw new Exception("Contato não existe.");
+        }
+        return contato_optional.get();
+    }
+
+    public Contato criarContato(Contato contato){
+        cr.save(contato);
+        return contato;
+    }
+
+    public Contato atualizarContato(Contato contato) throws Exception{
+        Optional<Contato> contato_optional = cr.findById(contato.getId());
+        if(contato_optional.isEmpty()){
+            throw new Exception("Contato não existe.");
+        }
+        cr.save(contato);
+        return contato;
+    }
+
+    public void deletarContato(Contato contato) throws Exception{
+        Optional<Contato> contato_optional = cr.findById(contato.getId());
+        if(contato_optional.isEmpty()){
+            throw new Exception("Contato não existe.");
+        }
+        cr.delete(contato);
+    }
 }
--- biblioteca/src/main/java/com/jambodev/biblioteca/services/LivroService.java
+++ biblioteca/src/main/java/com/jambodev/biblioteca/services/LivroService.java
@@ -5,8 +5,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.jambodev.biblioteca.repositories.LivroRepository;
 import com.jambodev.biblioteca.entities.Livro;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class LivroService{
     @Autowired
-    LivroRepository repository;
+    LivroRepository lr;
+
+    public List<Livro> listarLivros(){
+        return lr.findAll();
+    }
+
+    public Livro buscarLivro(Long id) throws Exception{
+        Optional<Livro> livro_optional = lr.findById(id);
+        if(livro_optional.isEmpty()){
+            throw new Exception("Livro não existe.");
+        }
+        return livro_optional.get();
+    }
+
+    public Livro criarLivro(Livro livro){
+        lr.save(livro);
+        return livro;
+    }
+
+    public Livro atualizarLivro(Livro livro) throws Exception{
+        Optional<Livro> livro_optional = lr.findById(livro.getId());
+        if(livro_optional.isEmpty()){
+            throw new Exception("Livro não existe.");
+        }
+        lr.save(livro);
+        return livro;
+    }
+
+    public void deletarLivro(Livro livro) throws Exception{
+        Optional<Livro> livro_optional = lr.findById(livro.getId());
+        if(livro_optional.isEmpty()){
+            throw new Exception("Livro não existe.");
+        }
+        lr.delete(livro);
+    }
 }
