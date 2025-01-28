978377117c2fb5573848b6e351b908fb020423d7
esteroliver <oliver.ester.2004@gmail.com>
2025-01-28 18:11:16 -0300

    chore: configuração dos endpoints que não precisam de autenticação para serem acessados

---
+++ biblioteca/src/main/java/com/jambodev/biblioteca/configuration/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.jambodev.biblioteca.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
+        "/usuarios/login", //url que usaremos para fazer login
+        "/usuarios", //url que usaremos para criar um usuário
+        "/" //landing page
+    };
+}
