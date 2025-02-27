42e864d45be38bb1e43cb33ced1f0c9614fe6216
esteroliver <oliver.ester.2004@gmail.com>
2025-02-27 15:35:37 -0300

    feat: configuração das rotas

--- biblioteca-front/src/app/app.component.ts
+++ biblioteca-front/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { CadastroComponent } from './components/cadastro/cadastro.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet, 
-    LoginComponent,
-    CadastroComponent,
-    LandingPageComponent
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- biblioteca-front/src/app/app.routes.ts
+++ biblioteca-front/src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'cadastro',
+        component: CadastroComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
