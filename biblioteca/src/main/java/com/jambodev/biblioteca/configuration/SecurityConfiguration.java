6e836d96d02751388947e4d6da4e01558986a4f7
esteroliver <oliver.ester.2004@gmail.com>
2025-02-27 14:00:01 -0300

    feat: configurações de segurança

--- biblioteca/src/main/java/com/jambodev/biblioteca/configuration/SecurityConfiguration.java
+++ biblioteca/src/main/java/com/jambodev/biblioteca/configuration/SecurityConfiguration.java
@@ -1,14 +1,60 @@
 package com.jambodev.biblioteca.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.jambodev.biblioteca.component.UserAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
+    private UserAuthenticationFilter userAuthenticationFilter;
     public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
-        "/usuarios/login", //url que usaremos para fazer login
-        "/usuarios", //url que usaremos para criar um usuário
+        "/login", //url que usaremos para fazer login
+        "/cadastro", //url que usaremos para criar um usuário
         "/" //landing page
     };
+
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
+        "/livros",
+        "/livros/{id}",
+        "/contatos",
+        "/contatos/{id}",    
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorize -> 
+                    authorize
+                    .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                    .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
+                    .anyRequest().denyAll()
+        );
+
+        httpSecurity.addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        return auth.build();
+    }
+
+    @Bean 
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
