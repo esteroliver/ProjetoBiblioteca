2f0453cd5eb47e1f1b8079151ac2017328b0136e
esteroliver <oliver.ester.2004@gmail.com>
2025-02-27 14:56:56 -0300

    feat: movendo e adicionando os componentes

--- biblioteca-front/src/app/app.component.ts
+++ biblioteca-front/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from '../components/login/login.component';
-import { CadastroComponent } from '../components/cadastro/cadastro.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @Component({
   selector: 'app-root',
---
+++ biblioteca-front/src/app/components/estante/estante.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estante',
+  imports: [],
+  templateUrl: './estante.component.html',
+  styleUrl: './estante.component.css'
+})
+export class EstanteComponent {
+
+}
