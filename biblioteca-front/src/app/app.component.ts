eef1c7f7097eb4b7d2350a0d4d063d63a2e31c80
esteroliver <oliver.ester.2004@gmail.com>
2025-02-27 15:09:24 -0300

    style: landing page

--- biblioteca-front/src/app/app.component.ts
+++ biblioteca-front/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    LandingPageComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
